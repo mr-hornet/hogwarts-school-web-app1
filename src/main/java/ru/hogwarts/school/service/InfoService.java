e7cf1e99c89b904d942c14d6b4ca781b45f3c5dc
Alexandr <mr-hornet@mail.ru>
2023-07-10 00:18:18 +1000

    done

---
+++ src/main/java/ru/hogwarts/school/service/InfoService.java
@@ -0,0 +1,5 @@
+package ru.hogwarts.school.service;
+
+public interface InfoService {
+    String getPort();
+}
--- src/main/java/ru/hogwarts/school/service/impl/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/impl/FacultyServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import ru.hogwarts.school.exception.EntityNotFoundException;
@@ -16,6 +18,8 @@ import java.util.stream.Collectors;
 public class FacultyServiceImpl implements FacultyService {
 
     private final FacultyRepository facultyRepository;
+    private final Logger logger = LoggerFactory.getLogger(FacultyServiceImpl.class);
+
 
     public FacultyServiceImpl(FacultyRepository facultyRepository) {
         this.facultyRepository = facultyRepository;
@@ -24,11 +28,13 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Faculty add(Faculty faculty) {
+        logger.info("Was invoked method for add faculty");
         return facultyRepository.save(faculty);
     }
 
     @Override
     public Faculty remove(Long id) {
+        logger.info("Was invoked method for remove faculty");
         Faculty faculty = getById(id);
         facultyRepository.deleteById(id);
         return faculty;
@@ -36,6 +42,7 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Faculty update(Faculty faculty) {
+        logger.info("Was invoked method for update faculty");
         Faculty findFaculty = getById(faculty.getId());
         facultyRepository.save(faculty);
         return findFaculty;
@@ -43,22 +50,28 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Collection<Faculty> getAll() {
+        logger.info("Was invoked method for get all faculties");
         return facultyRepository.findAll();
     }
 
     @Override
     public Faculty getById(Long id) {
+        logger.info("Was invoked method for get by ID faculty");
         Optional<Faculty> faculty = facultyRepository.findById(id);
         if (faculty.isPresent()) {
             return faculty.get();
         } else {
+            logger.error("Faculty not found, id = " + id);
             throw new EntityNotFoundException();
         }
     }
 
     @Override
     public Collection<Faculty> getByNameOrColor(String name, String color) {
+        logger.info("Was invoked method for get by name or color faculty");
         if (!StringUtils.hasText(name) && !StringUtils.hasText(color)) {
+            logger.error("Incorrect arguments to find faculties");
+
             throw new IncorrectArgumentException();
         }
         return facultyRepository.findFacultiesByNameIgnoreCaseOrColorIgnoreCase(name, color);
